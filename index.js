a5b8d9931d4c4cd3a440dc713038a6384bb271b0
Ollie Bannister <olwiba@gmail.com>
2016-06-28 16:15:50 +1200

    Updates

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ xhr.get(endpoint, function (err, data) {
   console.log(data.body) // FYI: data.body is a string
   var myData = JSON.parse(data.body)
 
-  console.log('this is my data ', myData.latitude, myData.longitude)
+  console.log('this is my data ', typeof myData.latitude, typeof myData.longitude)
   myLat = myData.latitude
   myLong = myData.longitude
   console.log('This is after ', myLat, myLong)
