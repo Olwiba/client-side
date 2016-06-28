658892ee065fb6fbbac9c7d316deb382679aee42
Ollie Bannister <olwiba@gmail.com>
2016-06-28 15:06:47 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 var showLocation = require('./views/location.hbs')
-console.log('this is the new page')
+
+console.log('this is the newer page')
+
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 var target = document.getElementsByTagName('main')[0]
 target.innerHTML = showLocation()
+
+
 function findISS() {
 
 xhr.get(endpoint, function (err, data) {
@@ -15,9 +19,6 @@ xhr.get(endpoint, function (err, data) {
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
   var myData = JSON.parse(data.body)
-
-  console.log(myData)
-  console.log('the test : ', myData.name)
 
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
